81ad7f842fce395ced267d588316d080f041c20d
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-06-26 17:32:46 +0530

    states and AgeStat added

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,39 @@
 import React,{Component} from 'react';
 import {Form, FormControl, Button} from 'react-bootstrap';
 import './App.css';
+import AgeStats from './AgeStats'
+
 
 class App extends Component{
+    constructor(){
+        super();
+
+        this.state={
+            newDate: '',
+            birthday:'1976-07-12'
+        }
+    }
+
+    changeBirthday(){
+        this.setState({birthday: this.state.newDate});
+    }
+
+
     render(){
         return(
             <div className='App'>
                 <Form inline>
                     <h2>Input Your Birthday!</h2>
-                    <FormControl type="date">
+                    <FormControl 
+                    type="date"
+                    onChange={event=>this.setState({newDate: event.target.value})}
+                    >
                     </FormControl>
                     {" "}
-                    <Button>
+                    <Button onClick={()=>this.changeBirthday()}>
                         Submit
                     </Button>
+                    <AgeStats date={this.state.birthday}/>
                 </Form>
             </div>
         )
